70da5b59e62c46c765670f8971d8aecac5dc8d72
Bernardo Donadio <bcdonadio@bcdonadio.com>
2024-05-12 06:32:24 -0300

    Refactor the Yale lock code

--- ymc420.js
+++ ymc420.js
@@ -1,13 +1,7 @@
-const {lock, identify} = require('zigbee-herdsman-converters/lib/modernExtend');
+const {lock, lock_action, pincode, battery} = require('zigbee-herdsman-converters/lib/modernExtend');
 const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
 const tz = require('zigbee-herdsman-converters/converters/toZigbee');
-const exposes = require('zigbee-herdsman-converters/lib/exposes');
 const reporting = require('zigbee-herdsman-converters/lib/reporting');
-const ota = require('zigbee-herdsman-converters/lib/ota');
-const utils = require('zigbee-herdsman-converters/lib/utils');
-const globalStore = require('zigbee-herdsman-converters/lib/store');
-const e = exposes.presets;
-const ea = exposes.access;
 
 const fzLocal = {
   action_source_name: {
@@ -15,22 +9,22 @@ const fzLocal = {
     type: "raw",
     convert: (model, msg, publish, options, meta) => {
       const lookup = {
-          0: 'password_unlock', //'unknown', // password 15/07
-          1: 'unlock', //'lock', unlock system 15/07
-          2: 'auto_lock', // 'unlock', // one touch lock and autolock 15/07
-          3: 'RFID_unlock', //'lock_failure_invalid_pin_or_id', // card 15/07
-          4: 'fingerprint_unlock', //'lock_failure_invalid_schedule',  // fingerprint 15/07
-          5: 'unlock_failure_invalid_pin_or_id',
-          6: 'unlock_failure_invalid_schedule',
-          7: 'one_touch_lock',
-          8: 'key_lock',
-          9: 'key_unlock',
-          10: 'auto_lock',
-          11: 'schedule_lock',
-          12: 'schedule_unlock',
-          13: 'manual_lock',
-          14: 'manual_unlock',
-          15: 'non_access_user_operational_event',
+        0: 'password_unlock',
+        1: 'unlock',
+        2: 'auto_lock',
+        3: 'RFID_unlock',
+        4: 'fingerprint_unlock',
+        5: 'unlock_failure_invalid_pin_or_id',
+        6: 'unlock_failure_invalid_schedule',
+        7: 'one_touch_lock',
+        8: 'key_lock',
+        9: 'key_unlock',
+        10: 'auto_lock',
+        11: 'schedule_lock',
+        12: 'schedule_unlock',
+        13: 'manual_lock',
+        14: 'manual_unlock',
+        15: 'non_access_user_operational_event',
       };
       const value = lookup[msg.data[3]];
       return { action_source_name: value }
@@ -45,28 +39,36 @@ const fzLocal = {
   },
 };
 
-
-const lockExtend = (meta) => {
-  return {
-    fromZigbee: [fzLocal.action_source_user, fzLocal.action_source_name, fz.lock, fz.battery, fz.lock_operation_event, fz.lock_programming_event, fz.lock_pin_code_response, fz.lock_user_status_response],
-    toZigbee: [tz.lock, tz.pincode_lock, tz.lock_userstatus],
-    meta: {pinCodeCount: 250, ...meta},
-    exposes: [e.lock(), e.lock_action(), e.battery(), e.pincode()],
-    configure: async (device, coordinatorEndpoint, logger) => {
-      const endpoint = device.getEndpoint(1);
-      await reporting.bind(endpoint, coordinatorEndpoint, ['closuresDoorLock', 'genPowerCfg']);
-      await reporting.lockState(endpoint);
-      await reporting.batteryPercentageRemaining(endpoint);
-    },
-  };
-};
-
-module.exports = [
-  {
-    zigbeeModel: ['YMC420'],
-    model: 'YMC420-W-PFF',
-    vendor: "Yale",
-    description: "YMC420",
-    extend: lockExtend(),
+module.exports = {
+  zigbeeModel: ['YMC420'],
+  model: 'YMC420-W-PFF',
+  vendor: "Yale",
+  description: "YMC420",
+  fromZigbee: [
+    fzLocal.action_source_user,
+    fzLocal.action_source_name,
+    fz.lock, fz.battery,
+    fz.lock_operation_event,
+    fz.lock_programming_event,
+    fz.lock_pin_code_response,
+    fz.lock_user_status_response
+  ],
+  toZigbee: [
+    tz.lock,
+    tz.pincode_lock,
+    tz.lock_userstatus
+  ],
+  meta: {pinCodeCount: 250, ...meta},
+  exposes: [
+    lock(), 
+    lock_action(), 
+    battery(), 
+    pincode(),
+  ],
+  configure: async (device, coordinatorEndpoint) => {
+    const endpoint = device.getEndpoint(1);
+    await reporting.bind(endpoint, coordinatorEndpoint, ['closuresDoorLock', 'genPowerCfg']);
+    await reporting.lockState(endpoint);
+    await reporting.batteryPercentageRemaining(endpoint);
   },
-];
+};
